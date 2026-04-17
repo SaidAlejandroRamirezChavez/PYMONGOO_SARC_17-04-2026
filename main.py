a45a42b779eabe14c6894d66168c98305bad75c1
SaidAlejandroRamirezChavez <24308060610069@cetis61.edu.mx>
2026-04-17 12:25:34 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="template")
+
+@app.route("/")
+def index():
+    return render_template("inicio.html")
+
+@app.route("/Gestor")
+def Tareas():
+    return render_template("GestordeTareas.html")
+
+@app.route("/secion")
+def secion():
+    return render_template("inicio_secion.html")
+
+@app.route("/cuenta")
+def inisiar():
+    return render_template("formulario.html")
+
+@app.route("/recuperar")
+def Contraseña():
+    return render_template("recuperar_contra.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
